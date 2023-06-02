b6f1698d083570d71c05fcbe8f8b66442ba9574d
Sasha_work <kalinovsasha@yandex.ru>
2023-06-02 16:19:40 +0300

    feat: add class card

---
+++ taro/src/components/card.ts
@@ -0,0 +1,51 @@
+import { TaroCard } from '../model/cards';
+import './card.scss';
+
+export default class Card {
+  element: HTMLElement | null = null
+  rootElement: HTMLElement | null = null
+  constructor(rootElement: HTMLElement, styles: string[] = [], card: TaroCard) {
+    this.rootElement = rootElement;
+
+    this.element = document.createElement("div");
+    this.element.classList.add('taro-card');
+    this.element.classList.add(...styles);
+
+    const title = document.createElement("h3");
+    title.classList.add('taro-card__title');
+    title.innerText = card.value;
+    this.element.append(title)
+
+    const container = document.createElement("div");
+    container.classList.add('taro-card__container');
+    this.element.append(container)
+
+
+    const img = document.createElement("img");
+    img.classList.add('taro-card__img');
+    img.setAttribute('src', card.image);
+    container.append(img)
+
+    const descr = document.createElement("p");
+    descr.classList.add('taro-card__desc');
+    descr.innerText = card.descr;
+    descr.innerText += "\n\n" + card.direct;
+    container.append(descr)
+
+    rootElement.append(this.element);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+
+  }
+
+  render() {
+    if (this.element && this.rootElement) {
+      this.rootElement.append(this.element);
+    }
+
+  }
+}
--- taro/src/main.ts
+++ taro/src/main.ts
@@ -1,9 +1,12 @@
 import './style.css'
 import shufle from './scripts/utils/shufle'
 import {cards} from "./model/cards"
+import Card from './components/card';
 
 let div = document.getElementById('app') as HTMLElement ;
 const cards2 = shufle(cards);
 for(let i = 0; i < 78; i++){
-  div.insertAdjacentHTML('beforebegin', `<img src=${cards[i].image} alt="${cards[i].value}">`);
+ //div.insertAdjacentHTML('beforebegin', `<img src=${cards[i].image} alt="${cards[i].value}">`);
 }
+
+const tst = new Card(document.getElementById("app")!,['card'],cards[0])
